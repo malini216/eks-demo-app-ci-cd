16eb9b56d9c99753936ae0ddc73e2bba44cc7efd
malini216 <109737980+malini216@users.noreply.github.com>
2022-11-17 16:04:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - welcome to project')
+  res.send('Hello World! - welcome to ICU project')
 })
 
 app.listen(port, () => {
